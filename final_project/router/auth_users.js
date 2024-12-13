b41c3f29f0a0ac0865f6c7abeade5b838ee61b73
Greg Lorenzen <gregorylorenzen@gmail.com>
2024-12-13 15:54:48 -0800

    Fix isValide function

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -10,6 +10,19 @@ const isValid = (username) => {
   let userswithsamename = users.filter((user) => user.username === username);
   // Return true if any user with the same username is found, otherwise false
   if (userswithsamename.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const authenticatedUser = (username, password) => {
+  // Filter the users array for any user with the same username and password
+  let validusers = users.filter((user) => {
+    return user.username === username && user.password === password;
+  });
+  // Return true if any valid user is found, otherwise false
+  if (validusers.length > 0) {
     return true;
   } else {
     return false;
