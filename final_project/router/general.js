827a4dd85a6efd67316a9fde8bc1828a70032dbc
Greg Lorenzen <gregorylorenzen@gmail.com>
2024-12-13 15:43:44 -0800

    Add route for finding by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,8 +24,10 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author;
+  const allBooks = Object.values(books);
+  const matchingBook = allBooks.filter((book) => book.author === author);
+  return res.status(200).send(matchingBook);
 });
 
 // Get all books based on title
